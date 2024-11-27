82121965d62e4989d9aa473c79a705f92c7afd40
Muhsinullah <muhsinkhanj786@gmail.com>
2024-11-27 12:05:10 +0500

    initiat

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -102,17 +102,6 @@ const Footer = () => {
 
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
-            <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              Shop.co © Made by 
-              <Link
-                href="#"
-                className="text-black font-medium"
-              >
-                Bilal Shaheen
-              </Link>
-              {", "}
-              Designed by Shaheen
-            </p>
             <div className="flex items-center">
               {paymentBadgesData.map((badge, _, arr) => (
                 <span
